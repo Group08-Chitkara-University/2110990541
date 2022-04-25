20859ae03ca52410b98048f1cf7efd2b9df84757
haraksh singh <haraksh0541.be21@chitkara.edu.in>
2022-04-25 17:22:38 +0530

    commit of calculator program

---
+++ sample.cpp
@@ -0,0 +1,84 @@
+
+
+C
+//this comment made in vi editor and this is a calculator
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+class calculator {
+public:
+string Operator;
+int num1;
+int num2;
+int result;
+int add(int num1,int num2);
+int subtract(int num1,int num2);
+int multiply(int num1,int num2);
+int divide(int num1,int num2);
+int remainder(int num1,int num2);
+int exponent(int num1,int num2);
+};
+
+int calculator::add(int num1,int num2) {
+return num1+num2;
+}
+int calculator::subtract(int num1,int num2) {
+return num1-num2;
+}
+int calculator::multiply(int num1,int num2) {
+return num1*num2;
+}
+int calculator::divide(int num1,int num2) {
+return num1/num2;
+}
+int calculator::exponent(int num1,int num2) {
+int output=1;
+for (int i=0;i<num2;i++) {
+output=output*num1;
+}
+return output;
+}
+int calculator::remainder(int num1,int num2) {
+return num1%num2;
+}
+int main() {
+calculator obj;
+cout << "enter the first number: ";
+cin >> obj.num1;
+cout << "enter the second number: ";
+cin >> obj.num2;
+cout << "enter the operator: ";
+cin >> obj.Operator;
+if (obj.Operator=="+") {
+obj.add(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.add(obj.num1,obj.num2);
+}
+
+else if (obj.Operator=="-") {
+obj.subtract(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.subtract(obj.num1,obj.num2);
+}
+
+else if (obj.Operator=="*") {
+obj.multiply(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.multiply(obj.num1,obj.num2);
+}
+else if (obj.Operator=="/") {
+obj.divide(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.divide(obj.num1,obj.num2);
+}
+else if (obj.Operator=="%") {
+obj.remainder(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.remainder(obj.num1,obj.num2);
+}
+else if (obj.Operator=="^") {
+obj.exponent(obj.num1,obj.num2);
+cout << "your opreration is: " << obj.num1 << " " << obj.Operator << " " << obj.num2 << " = " << obj.exponent(obj.num1,obj.num2);
+}
+return 0;
+}
