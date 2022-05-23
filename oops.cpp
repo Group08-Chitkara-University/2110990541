5d7a6becc16d465fdc684b65146ab528e38b395c
HarishDharpure <harishdharpure79@gmail.com>
2022-05-23 12:49:37 +0530

    harish maded changes in oops.cpp

--- oops.cpp
+++ oops.cpp
@@ -1,3 +1,4 @@
+//changes is being maked me harish
 #include <iostream>
 using namespace std;
 
@@ -14,9 +15,10 @@ cout << "name of student: " << obj.name << "\nage of student: " << obj.age << "\
 
 int main() {
     myClass obj;
-    obj.name="haraksh singh";
+    bj.name="haraksh singh";
     obj.age=19;
     obj.show();
     obj.dob="13 03 2003";
     return 0;
+    cout<<"class are defined outside main() function"<<endl;	
 };
